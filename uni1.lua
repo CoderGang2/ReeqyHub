cabf9d7071d5f5d4c229d433d13a412965f1ab9a
Ivory <103183001+IvoryXL@users.noreply.github.com>
2022-07-11 19:51:56 +0100

    Update uni1.lua

--- uni1.lua
+++ uni1.lua
@@ -9,3 +9,15 @@ JbSection:NewButton("Vynixius", "Vynixius - The Jailbreak Script", function()
     -- Execute the Vynixius script
     loadstring(game:HttpGet("https://raw.githubusercontent.com/RegularVynixu/Vynixius/main/Loader.lua"))()
 end)
+
+JbSection:NewButton("Auto Farm", "Auto Farm - The Jailbreak Script", function()
+    -- Execute the Auto Farm script
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/wawsdasdacx/ohascriptnrrewading/main/jbsaxscriptidk1"))()
+end)
+
+-- Credits Tab
+local CreditsTab = Window:NewTab("Credits")
+local CreditsSection = CreditsTab:NewSection("Credits")
+
+local TextBox = CreditsSection:NewTextBox("Credits")
+TextBox:SetText("Created by Ivory")
