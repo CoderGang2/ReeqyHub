4ebbe1ac91b666f53ecdb874245acb9a8185140f
Ivory <103183001+IvoryXL@users.noreply.github.com>
2022-07-11 17:03:05 +0100

    Auto Script Create your mom lol im so funny ha ha ha ha if you are not laughing i will come to your house and fuck your mom

---
+++ auto.lua
@@ -0,0 +1,7 @@
+if game.PlaceId == 606849621 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/RegularVynixu/Vynixius/main/Loader.lua"))()
+end
+
+if game.PlaceId == 2788229376 then
+    loadstring(game:HttpGet('https://raw.githubusercontent.com/lerkermer/lua-projects/master/BETA_Swagmode'))()
+end
--- main.lua
+++
@@ -1,3 +0,0 @@
-if game.PlaceId == 606849621 then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/RegularVynixu/Vynixius/main/Loader.lua"))()
-end
