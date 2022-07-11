223f186be4e68d7078c597f27ced01bc09b1cc18
Ivory <103183001+IvoryXL@users.noreply.github.com>
2022-07-11 19:48:40 +0100

    Create uni1.lua

---
+++ uni1.lua
@@ -0,0 +1,11 @@
+local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"))()
+local Window = Library.CreateLib("TITLE", "Synapse")
+
+-- Jailbreak Tab
+local JbTab = Window:NewTab("Jailbreak")
+local JbSection = JbTab:NewSection("Jailbreak Scripts")
+
+JbSection:NewButton("Vynixius", "Vynixius - The Jailbreak Script", function()
+    -- Execute the Vynixius script
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/RegularVynixu/Vynixius/main/Loader.lua"))()
+end)
