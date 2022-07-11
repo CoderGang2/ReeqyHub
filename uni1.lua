71883380a0e0466ea69ff8ef99fb693f414fe2a7
Ivory <103183001+IvoryXL@users.noreply.github.com>
2022-07-11 20:08:43 +0100

    Update uni1.lua

--- uni1.lua
+++ uni1.lua
@@ -15,5 +15,32 @@ JbSection:NewButton("Auto Farm", "Auto Farm - The Jailbreak Script", function()
     loadstring(game:HttpGet("https://raw.githubusercontent.com/wawsdasdacx/ohascriptnrrewading/main/jbsaxscriptidk1"))()
 end)
 
+-- Dahood tab
+local DahoodTab = Window:NewTab("DaHood")
+local DahoodSection = DahoodTab:NewSection("DaHood Scripts")
+
+-- Create a button that launches SwagMode script
+DahoodSection:NewButton("SwagMode", "SwagMode - The DaHood Script", function()
+    -- Execute the SwagMode script
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Dahood/SwagMode/master/SwagMode.lua"))()
+end)
+
+-- Create a button that launches the InfiniteYield script
+DahoodSection:NewButton("InfiniteYield", "InfiniteYield - The DaHood Script", function()
+    -- Execute the InfiniteYield script
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source"))()
+end)
+
+-- Create a button that launches the NovaHub script
+DahoodSection:NewButton("NovaHub", "NovaHub - The DaHood Script", function()
+    -- Execute the NovaHub script
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Dahood/NovaHub/master/NovaHub.lua"))()
+end)
+
+-- Create a button that launches the faded script
+DahoodSection:NewButton("Faded", "Faded - The DaHood Script", function()
+    -- Execute the Faded script
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Dahood/Faded/master/Faded.lua"))()
+end)
 
 -- loadstring(Game:HttpGet("https://raw.githubusercontent.com/CoderGang2/ReeqyHub/main/uni1.lua"))()
